7b50fc00db77abeb7afe294ac3461fbc55935dfa
liek944 <185925440+liek944@users.noreply.github.com>
2025-03-21 15:09:05 +0800

    first commit

---
+++ caesar_cipher.rb
@@ -0,0 +1,14 @@
+def caesar_cipher(string, shift)
+  result = ''
+  string.each_char do |char|
+  if char.match?(/[A-Za-z]/)
+      ascii_offset = char.match?(/[A-Z]/) ? 65 : 97
+     shifted_char = ((char.ord - ascii_offset + shift)% 26 + ascii_offset).chr
+      result += shifted_char
+    else
+      result += char
+    end
+  end
+ result
+end
+puts caesar_cipher("hello world", 3)
